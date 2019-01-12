2040bd6c5671afb12c9269f5ef7834d0a20d8beb
mabdullahbari <44468825+mabdullahbari@users.noreply.github.com>
2019-01-12 10:34:28 -0600

    Update cachematrix.R
    As per the instruction in the assignment function was written

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y # it caches y argument and set to special envir by <<- 
+    inv <<- NULL # it clears out previous inverted values
+  }
+  get <- function() x #gets cached value
+  setInverse <- function(inverse) inv <<- inverse # saves inverted values to envir via <<-
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse() ##gets cached inverse via passed in function makeCacheMatrix, and assigned to inv
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
